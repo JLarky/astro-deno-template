097220e1bdc7d286311133ab450cbe2477f5ddad
JLarky <jlarky@gmail.com>
2023-06-09 21:29:37 -0600

    deno?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import deno from "npm:@astrojs/deno";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "hybrid",
+  adapter: deno(),
+});
